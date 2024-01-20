7d44b4e956c61d86ede30d2db05e76b2a356e115
LOGIKA <danilltrotsenko2002@gmail.com>
2024-01-20 14:19:36 +0200

    1

--- sprites.py
+++ sprites.py
@@ -8,19 +8,24 @@ class Sprite:
         self.step = 5
         self.rect = pygame.Rect(x, y, 64, 64)
         self.image = pygame.image.load(filename)
-
+        self.shot_sound = pygame.mixer.Sound('shot.mp3')
+        self.shot_sound.set_volume(0.2)
+        self.reloading = False
     def draw(self, scr):
         scr.blit(self.image, (self.rect.x, self.rect.y))
 
-    def move(self, win_width, win_height):
+    def control(self, win_width, win_height):
         keys = pygame.key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            pass
-        self.rect.x -= self.step
-        if keys[K_RIGHT] and self.rect.x < win_width - 64:
+        if keys[pygame.K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.step
+        if keys[pygame.K_RIGHT] and self.rect.x < win_width - 64:
             self.rect.x += self.step
-        if keys[K_UP] and self.rect.y > 5:
+        if keys[pygame.K_UP] and self.rect.y > 5:
             self.rect.y -= self.step
-        if keys[K_DOWN] and self.rect.y < win_height - 64:
+        if keys[pygame.K_DOWN] and self.rect.y < win_height - 64:
             self.rect.y += self.step
+        if keys[pygame.K_SPACE]:
+            if not self.reloading:
+                self.shot_sound.play()
+                self.reloading = True
 
