14d19ea666af64cb503e494a75fb4355161dd9e2
LOGIKA <danilltrotsenko2002@gmail.com>
2024-01-20 14:02:22 +0200

    1

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import pygame.transform
+
 from sprites import *
 pygame.init()
 
-screen = pygame.display.set_mode((1280, 720))
+width = 700
+height = 680
+
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
-player = Sprite(600, 400, "player-img.png")
+player = Sprite(width/2 - 32, height - 64, "player-img.png")
+bg_image = pygame.image.load('images.png')
+bg_image = pygame.transform.scale(bg_image, (width, height))
 
 while running:
 
@@ -13,13 +20,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill("purple")
+    screen.blit(bg_image, (0, 0))
 
+    player.move(width, height)
     player.draw(screen)
 
     pygame.display.flip()
     clock.tick(60)
 
 pygame.quit()
-
-print(1)
