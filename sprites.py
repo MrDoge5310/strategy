44186641d7881c657d42326b567ed4c37f721d11
LOGIKA <danilltrotsenko2002@gmail.com>
2024-01-20 14:02:22 +0200

    1

--- sprites.py
+++ sprites.py
@@ -11,3 +11,16 @@ class Sprite:
 
     def draw(self, scr):
         scr.blit(self.image, (self.rect.x, self.rect.y))
+
+    def move(self, win_width, win_height):
+        keys = pygame.key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            pass
+        self.rect.x -= self.step
+        if keys[K_RIGHT] and self.rect.x < win_width - 64:
+            self.rect.x += self.step
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.step
+        if keys[K_DOWN] and self.rect.y < win_height - 64:
+            self.rect.y += self.step
+
